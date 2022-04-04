acf78a2c3ec4898b468a1267ba1551f97df80b51
BesokDev <74706532+BesokDev@users.noreply.github.com>
2022-04-04 15:02:42 +0200

    Create navbar + Create entities + add Gedmo/doctrine-extensions
    Découverte des "traits" PHP

---
+++ src/Entity/Commande.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=CommandeRepository::class)
+ */
+class Commande
+{
+    # composer require gedmo/doctrine-extensions
+    # => Permet d'utiliser les class de Gedmo, pour avoir les propriétés de type datetime (createdAt, updatedAt, deletedAt)
+    use TimestampableEntity;
+    use SoftDeleteableEntity;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5)
+     */
+    private $quantity;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $total;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $state;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(string $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getTotal(): ?string
+    {
+        return $this->total;
+    }
+
+    public function setTotal(string $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
